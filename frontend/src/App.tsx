01ea40e49b44bdc909a0a555e46bca9525bd79e0
tejasnaik <naik.tejas11@gmail.com>
2025-06-16 22:06:42 -0400

    added Header files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import Header from './Header.tsx'
+
 
 function App() {
   return (
     <>
-
+      <Header/>
     </>
   )
 }
---
+++ frontend/src/Header.tsx
@@ -0,0 +1,9 @@
+import './Header.css'
+
+function Header() {
+    return(
+        <h1>Tennis-Coach-AI 🎾</h1>
+    );
+}
+
+export default Header
