aba911e3941f999dcd0bf328bf191d519d8b300a
tejasnaik <naik.tejas11@gmail.com>
2025-06-18 22:35:52 -0400

    adjusted frontend

--- frontend/src/InputQuery.tsx
+++ frontend/src/InputQuery.tsx
@@ -1,12 +1,25 @@
 import './InputQuery.css'
+import React, { useState, useRef } from "react";
 
 function InputQuery() {
+    const [text, setText] = useState("");
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setText(e.target.value);
+        if (textareaRef.current) {
+            textareaRef.current.style.height = "auto";
+            textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+        }
+    };
     return(
         <>
+        <div id="inputquery-container">
         <button id="mute-button">Mute</button>
-        <input type="text" id="chat-input" placeholder="Ask Me Anything..." />
-        <button id="send-button">↑</button>
+        <textarea id="chat-input" ref={textareaRef} value={text} onChange={handleChange} rows={1} placeholder="Ask me anything..."/>
+        { text && <button id="send-button">↑</button> }
         <button id="clear-button">Clear</button>
+        </div>
         </>
     );
 }
