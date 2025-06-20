37275ea4b863fe30eb67f8e6c2868897e7fe6095
tejasnaik <naik.tejas11@gmail.com>
2025-06-19 20:30:58 -0400

    added final component image and message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import Header from './Header.tsx'
 import InputQuery from './InputQuery.tsx'
+import ChatBox from './ChatBox.tsx'
 
 
 function App() {
   return (
     <>
-      <Header/>
-      <InputQuery/>
+      <Header />
+      <ChatBox />
+      <InputQuery />
     </>
   )
 }
---
+++ frontend/src/ChatBox.tsx
@@ -0,0 +1,11 @@
+import './ChatBox.css'
+import tennisBall from './assets/TennisBall.png'
+
+
+function ChatBox() {
+    return(<>
+    <img src={tennisBall} alt="Tennis ball" />
+    </>);
+}
+
+export default ChatBox
