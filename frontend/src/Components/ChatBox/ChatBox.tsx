049733f17b5f12c411b636ef2958ae430bc9ff76
tejasnaik <naik.tejas11@gmail.com>
2025-06-22 00:25:54 -0400

    added div for message system

--- frontend/src/Components/ChatBox/ChatBox.tsx
+++ frontend/src/Components/ChatBox/ChatBox.tsx
@@ -1,7 +1,11 @@
 import './ChatBox.css'
 
 function ChatBox() {
-    return(<></>);
+    return(
+        <div id="chat-box">
+            <div className="message system-message">How can I help you?</div>
+        </div>
+    );
 }
 
 export default ChatBox
