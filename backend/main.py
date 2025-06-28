5b9391fce7251414826176843ad7bd5231f27a02
tejasnaik <naik.tejas11@gmail.com>
2025-06-28 17:14:54 -0400

    started backend huggingface

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import requests
-
-app = Flask(__name__)
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+from huggingface_hub import InferenceClient
